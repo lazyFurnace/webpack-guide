f8a82dddbb1485da76676b65f555c516d75c7ff3
qi.li <787139384@qq.com>
2018-06-10 23:19:05 +0800

    有一个页面上了我自己的轮播图插件

--- src/routerPage/App.js
+++ src/routerPage/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import RotationMap from 'react-rotation-map';
 
 import style from './App.less';
 
 export default class App extends React.Component {
     render () {
         return (
-            <div className="react-start">
-                Hello React Router
-                <p className={style.build}>这是Router页面!</p>
+            <div className={style.father}>
+                <RotationMap>
+                    {
+                        [
+                            <div className={style.test}>111</div>,
+                            <div className={style.test}>222</div>,
+                            <div className={style.test}>333</div>,
+                            <div className={style.test}>444</div>
+                        ]
+                    }
+                </RotationMap>
             </div>
         )
     }
