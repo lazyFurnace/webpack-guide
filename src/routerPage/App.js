1739aff4d3df70c1cc767108af02089a51d07c77
qi.li <787139384@qq.com>
2018-06-10 23:22:40 +0800

    key值添加

--- src/routerPage/App.js
+++ src/routerPage/App.js
@@ -10,10 +10,10 @@ export default class App extends React.Component {
                 <RotationMap>
                     {
                         [
-                            <div className={style.test}>111</div>,
-                            <div className={style.test}>222</div>,
-                            <div className={style.test}>333</div>,
-                            <div className={style.test}>444</div>
+                            <div key="111" className={style.test}>111</div>,
+                            <div key="222" className={style.test}>222</div>,
+                            <div key="333" className={style.test}>333</div>,
+                            <div key="444" className={style.test}>444</div>
                         ]
                     }
                 </RotationMap>
