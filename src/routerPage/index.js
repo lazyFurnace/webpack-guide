f23187b9b82f309da11c5195051f6f01bc048910
qi.li <787139384@qq.com>
2018-07-15 22:03:59 +0800

    热模块加载有问题  删除

--- src/routerPage/index.js
+++ src/routerPage/index.js
@@ -5,11 +5,6 @@ import '../common/js/common.js';
 
 import App from './App.js';
 
-//开启热加载
-if (module.hot) {
-    module.hot.accept();
-}
-
 ReactDom.render(
     <App/>,
     document.getElementById("root")
