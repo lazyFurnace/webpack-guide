01cdf826c227d578ae819ea829e49d7018348c74
qi.li <787139384@qq.com>
2018-10-09 22:33:38 +0800

    update: 更新轮播图 npm 包

--- src/routerPage/App.js
+++ src/routerPage/App.js
@@ -8,14 +8,10 @@ export default class App extends React.Component {
         return (
             <div className={style.father}>
                 <RotationMap>
-                    {
-                        [
-                            <div key="111" className={style.test}>111</div>,
-                            <div key="222" className={style.test}>222</div>,
-                            <div key="333" className={style.test}>333</div>,
-                            <div key="444" className={style.test}>444</div>
-                        ]
-                    }
+                    <div key="111" className={style.test}>111</div>
+                    <div key="222" className={style.test}>222</div>
+                    <div key="333" className={style.test}>333</div>
+                    <div key="444" className={style.test}>444</div>
                 </RotationMap>
             </div>
         )
