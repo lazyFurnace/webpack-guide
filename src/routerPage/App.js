0544be60bebedd09a81afdc847671542c425f620
qi.li <787139384@qq.com>
2019-01-18 02:50:42 +0800

    update

--- src/routerPage/App.js
+++ src/routerPage/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import RotationMap from 'react-rotation-map';
+import Rotation from 'react-rotation-map';
 
 import style from './App.less';
 
@@ -7,12 +7,12 @@ export default class App extends React.Component {
     render() {
         return (
             <div className={style.father}>
-                <RotationMap>
+                <Rotation>
                     <div key="111" className={style.test}>111</div>
                     <div key="222" className={style.test}>222</div>
                     <div key="333" className={style.test}>333</div>
                     <div key="444" className={style.test}>444</div>
-                </RotationMap>
+                </Rotation>
             </div>
         )
     }
